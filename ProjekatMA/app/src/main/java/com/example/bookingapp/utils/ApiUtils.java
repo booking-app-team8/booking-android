72ce898412fcf77ba632358a7cf52df584a2ceed
Luka Farkaš <105086132+lukafarkas02@users.noreply.github.com>
2024-08-10 14:12:26 +0200

    added base url for my hometown

--- ProjekatMA/app/src/main/java/com/example/bookingapp/utils/ApiUtils.java
+++ ProjekatMA/app/src/main/java/com/example/bookingapp/utils/ApiUtils.java
@@ -6,7 +6,8 @@ import com.example.bookingapp.services.IUserService;
 //192.168.1.5
 public class ApiUtils {
 
-    public static final String BASE_URL = "http://192.168.1.5:8081/api/";
+//    public static final String BASE_URL = "http://192.168.1.5:8081/api/" NOVI SAD WIFI;
+    public static final String BASE_URL = "http://192.168.0.15:8081/api/";
 
     public static IUserService getUserService(){
         return ApiClient.getClient(BASE_URL).create(IUserService.class);
