400afcbf2c8d105db11baef88b08f6bdf003cbef
bojangelic <bojangelic417@gmail.com>
2024-08-15 14:03:53 +0200

    Guest Main Activity started

--- ProjekatMA/app/src/main/java/com/example/bookingapp/activities/home/guest/GuestMainActivity.java
+++ ProjekatMA/app/src/main/java/com/example/bookingapp/activities/home/guest/GuestMainActivity.java
@@ -1,18 +1,116 @@
 package com.example.bookingapp.activities.home.guest;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.DatePicker;
+import android.widget.EditText;
+
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import com.example.bookingapp.R;
+import com.example.bookingapp.activities.startup.LogInActivity;
+import com.google.android.material.navigation.NavigationView;
+
+import java.util.Calendar;
 
 public class GuestMainActivity extends AppCompatActivity {
+    private DrawerLayout drawerLayout;
+    private ActionBarDrawerToggle toggle;
+    private EditText etStartDate, etEndDate;
+    private Calendar calendar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_guest_main);
 
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        etStartDate = findViewById(R.id.et_start_date);
+        etEndDate = findViewById(R.id.et_end_date);
+
+        calendar = Calendar.getInstance();
+
+        // Postavljanje listenera za otvaranje DatePicker-a za start date
+        etStartDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openDatePickerDialog(etStartDate);
+            }
+        });
+
+        // Postavljanje listenera za otvaranje DatePicker-a za end date
+        etEndDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openDatePickerDialog(etEndDate);
+            }
+        });
+
+        drawerLayout = findViewById(R.id.drawer_layout);
+        toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+
+        // Povezivanje sa NavigationView
+        NavigationView navigationView = findViewById(R.id.navigation_view);
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                int id = item.getItemId();
+
+                if (id == R.id.nav_home) {
+                    // Otvori Početna
+                } else if (id == R.id.nav_account) {
+                    // Otvori Nalog
+                } else if (id == R.id.nav_reservations) {
+                    // Otvori Rezervacije
+                } else if (id == R.id.nav_notifications) {
+                    // Otvori Notifikacije
+                }
+
+                drawerLayout.closeDrawers();
+                return true;
+            }
+        });
 
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.guest_main_menu, menu);
+        return true;
+    }
+
+    // Funkcija za otvaranje DatePickerDialog-a
+    private void openDatePickerDialog(final EditText editText) {
+        int year = calendar.get(Calendar.YEAR);
+        int month = calendar.get(Calendar.MONTH);
+        int day = calendar.get(Calendar.DAY_OF_MONTH);
+
+        DatePickerDialog datePickerDialog = new DatePickerDialog(
+                GuestMainActivity.this,
+                new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+                        // Formatiranje i prikaz odabranog datuma u EditText
+                        String selectedDate = dayOfMonth + "/" + (monthOfYear + 1) + "/" + year;
+                        editText.setText(selectedDate);
+                    }
+                },
+                year, month, day);
+        datePickerDialog.show();
+    }
+
+
 }
