d4df47aeee855a9c37d6182353b3b788350b05c6
bojangelic <bojangelic417@gmail.com>
2024-08-16 23:33:59 +0200

    Admin can approve accommodation request

---
+++ ProjekatMA/app/src/main/java/com/example/bookingapp/models/AccommodationRequest.java
@@ -0,0 +1,80 @@
+package com.example.bookingapp.models;
+
+public class AccommodationRequest {
+    private Long id;
+    private String name;
+    private String address;
+    private String city;
+    private String country;
+    private String photoUrl;
+    private String owner;
+
+    public AccommodationRequest() {
+    }
+
+    public AccommodationRequest(Long id, String name, String address, String city, String country, String photoUrl, String owner) {
+        this.id = id;
+        this.name = name;
+        this.address = address;
+        this.city = city;
+        this.country = country;
+        this.photoUrl = photoUrl;
+        this.owner = owner;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getPhotoUrl() {
+        return photoUrl;
+    }
+
+    public void setPhotoUrl(String photoUrl) {
+        this.photoUrl = photoUrl;
+    }
+
+    public String getOwner() {
+        return owner;
+    }
+
+    public void setOwner(String owner) {
+        this.owner = owner;
+    }
+}
