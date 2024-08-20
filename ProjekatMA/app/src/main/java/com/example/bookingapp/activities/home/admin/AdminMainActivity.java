5dc9427263110ecac47b8ef6a05a9c69ff7933fb
bojangelic <bojangelic417@gmail.com>
2024-08-20 19:17:39 +0200

    Admin main screen

---
+++ ProjekatMA/app/src/main/java/com/example/bookingapp/activities/home/admin/AdminMainActivity.java
@@ -0,0 +1,16 @@
+package com.example.bookingapp.activities.home.admin;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.bookingapp.R;
+
+public class AdminMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_admin_main);
+    }
+}
