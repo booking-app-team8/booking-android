bfd98f27a7dbee77dc5715fa9a6efa588fc29f23
bojangelic <bojangelic417@gmail.com>
2024-08-21 17:29:07 +0200

    Change

--- ProjekatMA/app/src/main/java/com/example/bookingapp/services/IGradesService.java
+++ ProjekatMA/app/src/main/java/com/example/bookingapp/services/IGradesService.java
@@ -8,6 +8,6 @@ import retrofit2.Call;
 import retrofit2.http.GET;
 
 public interface IGradesService {
-    @GET("ownerGrades/getAllOwnerGrades")
+    @GET("ownerGrades/getAllOwnersGrades")
     Call<List<OwnerGrade>> getAllOwnerGrades();
 }
